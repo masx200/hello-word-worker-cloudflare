df4d54ba978f5d3ecee44f836ccf85fdd25087d9
masx200 <34191203+masx200@users.noreply.github.com>
2022-07-08 12:26:17 +0800

    升级

--- src/index.ts
+++ src/index.ts
@@ -9,33 +9,36 @@
  */
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
+    // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+    // MY_KV_NAMESPACE: KVNamespace;
+    //
+    // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+    // MY_DURABLE_OBJECT: DurableObjectNamespace;
+    //
+    // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+    // MY_BUCKET: R2Bucket;
 }
 
 export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext
-  ): Promise<Response> {
-    const { url, headers, method } = request;
+    async fetch(
+        request: Request,
+        env: Env,
+        ctx: ExecutionContext
+    ): Promise<Response> {
+        const { url, headers, method } = request;
 
-    return new Response(
-      JSON.stringify({
-        url,
-        method,
-        headers: Object.fromEntries(headers),
-      }),
-      { headers: { 
-        "Strict-Transport-Security" :"max-age=31536000",
-        "content-type": "application/json" } }
-    );
-  },
+        return new Response(
+            JSON.stringify({
+                url,
+                method,
+                headers: Object.fromEntries(headers),
+            }),
+            {
+                headers: {
+                    "Strict-Transport-Security": "max-age=31536000",
+                    "content-type": "application/json",
+                },
+            }
+        );
+    },
 };
